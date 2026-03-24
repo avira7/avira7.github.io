6917f948828fd8014a72d4619d229d9d014a1c26
Advik Vira <advikvira@Adviks-MacBook-Air.local>
2026-03-24 16:47:31 -0400

    updated news information

--- src/constants/news.ts
+++ src/constants/news.ts
@@ -3,18 +3,15 @@ export interface NewsUpdate {
   title: string
   description: string
   url: string
-  date: string // ISO 8601 date string, e.g. "2026-03-17"
+  date: string
 }
 
 export const newsUpdates: NewsUpdate[] = [
-//   {
-//     title: "Georgia Tech press release on high-Ti lunar ilmenite ",
-//     url: "https://example.com/research/radiation-shielding",
-//     date: "[03/20/2026]",
-//   },
-
   {
-    title: "KSCO manuscript recieved the 2026 Sigma Xi Best Paper Award",
+    titleParts: [
+      { text: "KSCO manuscript in Nat. Comm.", url: "https://www.nature.com/articles/s41467-025-67394-0" },
+      { text: " received the 2026 Sigma Xi Best Paper Award", url: null },
+    ],
     url: "https://bpb-us-e1.wpmucdn.com/sites.gatech.edu/dist/0/283/files/2026/03/2026_SigmaXi_Research_Award_Winner.pdf",
     date: "[03/23/2026]",
   },
@@ -24,13 +21,12 @@ export const newsUpdates: NewsUpdate[] = [
     date: "[02/19/2026]",
   },
   {
-    title: "Manuscript accepted in Nature Communications on high-Ti lunar ilmenite",
+    title: "Manuscript accepted in Nat. Comm. on high-Ti lunar ilmenite",
     url: "https://www.researchsquare.com/article/rs-7190245/v1?redirect=/article/rs-7190245",
     date: "[02/09/2026]",
   },
-{
+  {
     title: "Successful defense of PhD thesis!",
     date: "[01/09/2026]",
   },
-  // Add more updates here as needed
 ];
